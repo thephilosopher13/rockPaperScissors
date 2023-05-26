75324473ee72ff179d3396fcbb924ec06152e37b
Coloqs <60373194+thephilosopher13@users.noreply.github.com>
2023-05-26 17:59:06 +0800

    Put playRockPaperScissors function inside addEventListener, rename score value, edit how score value added

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
 const rockPaperScissorsArray = ['rock', 'paper', 'scissors']
 
-var playerPoints = 0;
-var computerPoints = 0;
-let roundsPlayed = 0;
+const playerScore = document.querySelector('.playerScore')
+let playerScoreCount = 0
+const computerScore = document.querySelector('.computerScore')
+let computerScoreCount = 0
 var endGame = false;
 
 function getComputerChoice(array) {
@@ -11,6 +12,7 @@ function getComputerChoice(array) {
     return computerChoice
   }
 
+  // function getId actually is there to get the choice of the player
 function getId(e) {
   const getPlayerChoice = e.target.id;
   return getPlayerChoice
@@ -23,44 +25,19 @@ buttons.forEach(function(button) {
     let playerChoiceValue = getId(e);
     let computerChoiceValue = getComputerChoice(rockPaperScissorsArray);
 
-
+    if (playerChoiceValue == "paper" && computerChoiceValue == "scissors" || playerChoiceValue == "rock" && computerChoiceValue == "paper" || playerChoiceValue == "scissors" && computerChoiceValue == "rock") {
+      computerScoreCount++;
+      alert("You lose! " + computerChoiceValue + " beats " + playerChoiceValue + ".");
+    } else if (playerChoiceValue == "paper" && computerChoiceValue == "rock" || playerChoiceValue == "scissors" && computerChoiceValue == "paper" || playerChoiceValue == "rock" && computerChoiceValue == "scissors" ) {
+      playerScoreCount++;
+      alert("You Win! " + playerChoiceValue + " beats " + computerChoiceValue + ".");
+    } else if (playerChoiceValue == computerChoiceValue) {
+      alert("Draw!");
+    } else {
+      alert("Invalid Input");
+    }
   });
 });
-
-var computerChoice = getComputerChoice(rockPaperScissorsArray)
-
-
-
-// function playRockPaperScissors() {
-  
-//  if (playerChoice == "paper" && computerChoice == "scissors" || playerChoice == "rock" && computerChoice == "paper" || playerChoice == "scissors" && computerChoice == "rock") {
-//    computerPoints++;
-//    roundsPlayed++;
-//    alert("You lose! " + computerChoice + " beats " + playerChoice + ". Player Points: " + playerPoints + ", Computer Points: " + computerPoints + ". Rounds Played: " + roundsPlayed);
-//  } else if (playerChoice == "paper" && computerChoice == "rock" || playerChoice == "scissors" && computerChoice == "paper" || playerChoice == "rock" && computerChoice == "scissors" ) {
-//    playerPoints++;
- //   roundsPlayed++;
-//    alert("You Win! " + playerChoice + " beats " + computerChoice + ". Player Points: " + playerPoints + ", Computer Points: " + computerPoints + ". Rounds Played: " + roundsPlayed);
-//  } else if (playerChoice == computerChoice) {
-//    roundsPlayed++;
-//    alert("Draw! Both of you picked " + playerChoice + ". Player Points: " + playerPoints + ", Computer Points: " + computerPoints + ". Rounds Played: " + roundsPlayed);
-//  } else {
-//    alert("Invalid Input");
-//  }
-// } // 
-
-function gameFinished() {
-  if (endGame) {
-    document.getElementById("gameButton").disabled = true
-    if (playerPoints > computerPoints) {
-      return alert("You win the match!")
-    } else if (playerPoints < computerPoints) {
-      return alert("You lose the match!")
-    } else {
-      return alert("Match draw!")
-    }
-  }
-}
 
 
 
